f3e898de4e63add410d086d8f5ff22b0466344f3
Leonardo Amaral Alves <leonardoamaralalves1@gmail.com>
2025-03-27 20:34:11 -0300

    adicionando itens na calculadora

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,19 @@ function calcular (){
     // vamos criar duas variáveis 
     // JS as variáveis não possuem tipo
     // document.getElementById para obter os dados dos inputs do html
-    let mascote, homenagem
+    let mascote, homenagem, leite, kitAvulso, suplementoAvulso
 
     // recupera o valor do mascote digitado pelo usuário
     mascote = document.getElementById("mascote").value
     // recupera o valor da homenagem digitado pelo usuário
     homenagem = document.getElementById("homenagem").value
+     // recupera a quantidade de litros de leite digitado pelo usuário
+    leite = document.getElementById("leite").value
+    kitAvulso = document.getElementById("kitAvulso").value
+    suplementoAvulso = document.getElementById("suplementoAvulso").value
+    
 
-    let soma = Number(mascote) + Number(homenagem)
-    alert (soma)
+    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite)) + 
+                (30 * Number(kitAvulso)) + (15 * Number(suplementoAvulso))
+    document.getElementById("soma").innerHTML=`A soma é ${soma}`
 }
